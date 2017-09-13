7bd93ef263c842ccdf872df8c53e24dc0d047e33
tanawatmoss <31152612+tanawatmoss@users.noreply.github.com>
2017-09-13 20:35:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('num', 1);
+var num = app.get('num');
+console.log(num); //1
+
+app.use(function(req, res, next) {
+    console.log('Record timestamp');
+    next();
+});
+
+app.use(bodyParser.json());
+app.post('/calculator/rest/plus', function (req, res, next) {
+    var body = req.body;  
+	var x = body.x;
+	var y = body.y;
+	var result = 0;
+	var obj ={
+		"x" : x,
+		"y" : y,
+		"result x + y": x + y
+	}
+    res.json(obj); 
+});
+
+app.post('/calculator/rest/minus',function(req,res,next){
+    var body = req.body;
+    var x   =   body.x;
+    var y   =   body.y;
+    var result  =   0;
+    var obj = {
+        "x" : x,
+        "y" : y,
+        "result x - y" : x - y
+    }
+    res.json(obj);
+});
+
+app.post('/calculator/rest/multiply',function(req,res,next){
+    var body = req.body;
+    var x   =   body.x;
+    var y   =   body.y;
+    var result  =   0;
+    var obj = {
+        "x" : x,
+        "y" : y,
+        "result x * y" : x * y
+    }
+    res.json(obj);
+});
+
+app.get('/help', function(req, res, next) {
+    res.send('Help me');
+});
+
+app.listen(3000, function() {
+    console.log('Starting node on port 3000');
+});
